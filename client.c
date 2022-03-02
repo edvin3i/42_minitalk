6c32a9003a991257e3331ca983903460bdf5fdcb
edvin3i <edviniii@gmail.com>
2022-03-02 10:04:59 +0300

    added libft

--- client.c
+++ client.c
@@ -10,44 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "include/minitalk.h"
-
-static int	ft_check_lllim(unsigned long long n, int sign)
-{
-	if (n >= LLONGMAX && sign == 1)
-		return (-1);
-	if (n >= LLONGMAX + 1 && sign == -1)
-		return (0);
-	return (21);
-}
-
-int	ft_atoi(const char *str)
-{
-	int					sign;
-	int					len;
-	unsigned long long	result;
-
-	sign = 1;
-	len = 0;
-	result = 0;
-	while (*str == 0x20 || (0x09 <= *str && *str <= 0x0d))
-		str++;
-	if (*str == '-' || *str == '+')
-	{
-		if (*str == '-')
-			sign *= -1;
-		str++;
-	}
-	while ((*str) && 0x30 <= *str && *str <= 0x39)
-	{
-		result = result * 10 + *str - 0x30;
-		str++;
-		len++;
-	}
-	if (ft_check_lllim(result, sign) == 0 || ft_check_lllim(result, sign) == -1)
-		return ((int)ft_check_lllim(result, sign));
-	return ((int)result * sign);
-}
-
+#include "include/libft.h"
 
 #include <stdio.h>
 
--- server.c
+++ server.c
@@ -10,11 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "include/minitalk.h"
+#include "include/libft.h"
 
 #include <stdio.h>
+
 void	ft_sighandler(int signum, siginfo_t *signal, void *context)
 {
-	printf("I feel signal");
+	ft_printf("I feel a signal\n");
 	sleep(1);
 }
 
@@ -22,19 +24,19 @@ void	ft_sighandler(int signum, siginfo_t *signal, void *context)
 
 int main(int argc, char **argv)
 {
-	int 	pid;
+	int 	self_pid;
 	struct	sigaction sigact;
 
-	pid = getpid();
-	printf("PID: %d\n", pid);
-	while (1)
-	{
-		sigact.sa_flags = SA_SIGINFO;
-		sigact.sa_sigaction = ft_sighandler;
-		sigaction(SIGUSR1, &sigact, NULL);
-		sigaction(SIGUSR2, &sigact, NULL);
+	self_pid = getpid();
+	ft_printf("PID: %d\n", self_pid);
 
-	}
+	sigact.sa_flags = SA_SIGINFO;
+	sigact.sa_sigaction = ft_sighandler;
+	sigaction(SIGUSR1, &sigact, NULL);
+	sigaction(SIGUSR2, &sigact, NULL);
+
+	while (1)
+		usleep(100);
 
 	return 0;
 }
